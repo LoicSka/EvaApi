3073af00e517cfb4adeddb2edd340909a1a4d75f
loic shyaka <loicshyaka09@gmail.com>
2018-07-14 11:51:52 +0800

    Debugging loading error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
@@ -52,7 +52,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.default_url_options = { :host => ENV['MAIL_GUN_DOMAIN'] }
   config.action_mailer.delivery_method = :smtp
 
   # SMTP settings
